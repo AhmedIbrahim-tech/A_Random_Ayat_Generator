d8cac646ae145b7b9fc2e01b3f5e2ad67dbc047b
AhmedIbrahim-tech <ebrahema89859@gmail.com>
2022-06-04 21:30:26 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//Select DOM
+const quoteText = document.querySelector(".quote"),
+  quoteBtn = document.querySelector("button"),
+  authorName = document.querySelector(".name"),
+  copyBtn = document.querySelector(".copy"),
+  twitterBtn = document.querySelector(".twitter"),
+  synth = speechSynthesis;
+
+//Functions
+
+
+async function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading...";
+  
+  let numberinsurah = Math.floor(Math.random() * (6348  - 1 + 1)) + 1;
+  const api = "https://api.alquran.cloud/v1/ayah";
+  const response = await fetch(`${api}/${numberinsurah}`);
+  const data = await response.json();
+
+  quoteText.innerText = data.data.text;
+  authorName.innerText = data.data.surah.name || "" ;
+
+  quoteBtn.classList.remove("loading");
+  quoteBtn.innerText = "New Aya";
+}
+
+//Event Listeners
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", () => {
+  let twitterUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(twitterUrl, "_blank");
+});
+
+quoteBtn.addEventListener("click", randomQuote);
+
+window.onload = function () {
+  quoteBtn.click();
+};
